58a746cf11bd1a93baaa1f5fbe793ee28c42b00d
danieldrazek <daniel_2000@o2.pl>
2019-05-05 17:20:30 +0100

    add file Methods.java

---
+++ Methods.java
@@ -0,0 +1,26 @@
+
+package methods;
+
+import java.util.Scanner;
+
+public class Methods {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Enter the temperature: ");
+        Scanner sc = new Scanner(System.in);
+        double temperature = sc.nextDouble();
+        tempCheck(temperature);
+        
+    }
+    public static void tempCheck(double temperature){
+        if(temperature > 0){
+            System.out.println("Temperature " + temperature + " is positive");
+        }       
+        else{
+            System.out.println("Temperature " + temperature + " is negative");
+        }
+        
+    }
+    
+}
